a6fdf2f972a80aeccfd625b193fd3f04b31a61b6
Snir Shechter <snirshechter@gmail.com>
2018-05-08 18:04:30 +0300

    move envConfig to config.js, add axios basepath

--- template/config/config.sample.js
+++ template/config/config.sample.js
@@ -1,3 +1,36 @@
-import getWindowVariable from "./getWindowVariable";
+const BASE_PATH = getWindowVariable('BASE_PATH', 'https://barak.cloud.yo/iqoscrmbg/api/');
 
-export const BASE_PATH = getWindowVariable('BASE_PATH', 'https://lee.cloud.yo/iqostr/p2papi/');
+const envConfig = {
+	default: {
+		ga_id: '1',
+		site_base_path: '/'
+	},
+	production: {
+		ga_id: '2',
+		site_base_path: '/'
+	},
+	dev: {
+		ga_id: '3',
+		site_base_path: '/'
+	},
+	stage: {
+		ga_id: '4',
+		site_base_path: '/'
+	}
+};
+
+module.exports = {
+	BASE_PATH,
+	env: envConfig[process.env.NODE_ENV] || envConfig.default
+};
+
+
+function getWindowVariable(name, dv) {
+	if (typeof window === 'undefined')
+		return dv;
+	
+	if (window && name in window) {
+		return window[name];
+	}
+	return dv;
+}
--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -1,95 +1,80 @@
 const nodeExternals = require('webpack-node-externals');
 const i18nOptions = require('./plugins/i18n.js');
+const config = require('./config/config.js');
 
-const envConfig = {
- default: {
-	ga_id: '1',
-	site_base_path: '/'
- },
- production: {
-	ga_id: '2',
-	site_base_path: '/'
- },
- dev: {
-	ga_id: '3',
-	site_base_path: '/'
- },
- stage: {
-	ga_id: '4',
-	site_base_path: '/'
- }
-};
-const envCurrent = envConfig[process.env.NODE_ENV] || envConfig.default;
 
 module.exports = {
- env: {
-	config: envCurrent
- },
- head: {
-	title: '{{ name }}',
-	meta: [
-	 {charset: 'utf-8'},
-	 {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-	 {hid: 'description', name: 'description', content: '{{escape description }}'}
-	],
-	link: [
-	 {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
-	],
-	base: {
-	 href: envCurrent.site_base_path
+	env: {
+		config: config.env
 	},
-	script: [
-	 {src: envCurrent.site_base_path + 'config/master.js'},
-	]
- },
- css: [
-	'~/styles/main.scss',
- ],
- router: {
-	base: envCurrent.site_base_path
- },
- transition: 'fade',
- loading: {color: '#3B8070'},
- modules: [
-	'@nuxtjs/axios',
-	['nuxt-i18n', i18nOptions],
-	['@nuxtjs/google-analytics', {
-	 id: envCurrent.ga_id
-	}]
- ],
- plugins: [
-	'~/plugins/notify.js',
-	'~/plugins/monoaxios.js',
-	{src: '~/plugins/monocaptcha.js', ssr: false},
-	'~/plugins/veeValidate.js',
- ],
- build: {
-	vendor: ['babel-polyfill'],
-	babel: {
-	 presets: [
-		['vue-app', {
-		 useBuiltIns: true,
-		 targets: {ie: 11, uglify: true}
+	axios: {
+		browserBaseURL: config.BASE_PATH
+	},
+	head: {
+		title: '{{ name }}',
+		meta: [
+			{charset: 'utf-8'},
+			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
+			{hid: 'description', name: 'description', content: '{{escape description }}'}
+		],
+		link: [
+			{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+		],
+		base: {
+			href: config.env.site_base_path
+		},
+		script: [
+			{src: config.env.site_base_path + 'config/master.js'},
+		]
+	},
+	css: [
+		'~/styles/main.scss',
+	],
+	router: {
+		base: config.env.site_base_path
+	},
+	transition: 'fade',
+	loading: {color: '#3B8070'},
+	modules: [
+		'@nuxtjs/axios',
+		['nuxt-i18n', i18nOptions],
+		['@nuxtjs/google-analytics', {
+			id: config.env.ga_id
+		}]
+	],
+	plugins: [
+		'~/plugins/notify.js',
+		'~/plugins/monoaxios.js',
+		{src: '~/plugins/monocaptcha.js', ssr: false},
+		'~/plugins/veeValidate.js',
+	],
+	build: {
+		vendor: ['babel-polyfill'],
+		babel: {
+			presets: [
+				['vue-app', {
+					useBuiltIns: true,
+					targets: {ie: 11, uglify: true}
+				}
+				]
+			]
+		},
+		extend (webpackConfig, {isDev, isClient, isServer}) {
+			if (isDev && isClient) {
+				webpackConfig.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					loader: 'eslint-loader',
+					exclude: /(node_modules)/
+				})
+			}
+			if (isServer) {
+				webpackConfig.externals = [
+					nodeExternals({
+						whitelist: [/\.(?!(?:js|json)$).{1,5}$/i, /^monuxt/] // Whitelists all monuxt packages
+					})
+				]
+			}
 		}
-		]
-	 ]
-	},
-	extend (config, {isDev, isClient,isServer}) {
-	 if (isDev && isClient) {
-		config.module.rules.push({
-		 enforce: 'pre',
-		 test: /\.(js|vue)$/,
-		 loader: 'eslint-loader',
-		 exclude: /(node_modules)/
-		})
-	 }
-	 if(isServer){
-		config.externals = [
-		 nodeExternals({
-			whitelist: [/\.(?!(?:js|json)$).{1,5}$/i, /^monuxt/] // Whitelists all monuxt packages
-		 })
-		]
-	 }
 	}
- }
 };
